ded3b3db56f8468366dbbddf908f6fbb761142b2
ArcticNerd <66078316+ArcticNerd@users.noreply.github.com>
2022-05-03 23:02:59 -0300

    Add files via upload

---
+++ RenameCSVLooneyTunes.ps1
@@ -0,0 +1,25 @@
+﻿$nid = @()
+$epid = @()
+
+Import-Csv Z:\Serverfolders\Database\LooneyTunes.csv -Delimiter "," | ForEach-Object {
+    $nid += $_.N
+    $epid += $_.E
+    }
+
+$folder = Get-Item -Path .
+
+ls *`).mkv | Select-Object Name,FullName | ForEach-Object {
+    $oldname = $_.FullName
+    $findname = ($_.Name.Substring(0,($_.Name.Length - 33))).Substring(51)
+    if ($nid -contains $findname) {
+        $Where = [array]::IndexOf($nid, $findname)
+        Write-Host $oldname " will become: " "Looney Tunes - "  $epid[$Where] " - " $nid[$Where]
+        $newname = $folder.FullName + "\" +"Looney Tunes - " + $epid[$Where] + " - " + $nid[$Where] + ".mkv"
+        $test = Get-Item -Path $newname
+        if ($test.Exists) {
+        }
+        else {
+        Copy-Item -Path $oldname -Destination $newname
+        }
+    }
+}
